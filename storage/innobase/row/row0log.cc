thomasmatecki/server

commit 3cef4f8f0fc88ae5bfae4603d8d600ec84cc70a9

MDEV-515 Reduce InnoDB undo logging for insert into empty table
We implement an idea that was suggested by Michael 'Monty' Widenius
in October 2017: When InnoDB is inserting into an empty table or partition,
we can write a single undo log record TRX_UNDO_EMPTY, which will cause
ROLLBACK to clear the table.

For this to work, the insert into an empty table or partition must be
covered by an exclusive table lock that will be held until the transaction
has been committed or rolled back, or the INSERT operation has been
rolled back (and the table is empty again), in lock_table_x_unlock().

Clustered index records that are covered by the TRX_UNDO_EMPTY record
will carry DB_TRX_ID=0 and DB_ROLL_PTR=1<<55, and thus they cannot
be distinguished from what MDEV-12288 leaves behind after purging the
history of row-logged operations.

Concurrent non-locking reads must be adjusted: If the read view was
created before the INSERT into an empty table, then we must continue
to imagine that the table is empty, and not try to read any records.
If the read view was created after the INSERT was committed, then
all records must be visible normally. To implement this, we introduce
the field dict_table_t::bulk_trx_id.

This special handling only applies to the very first INSERT statement
of a transaction for the empty table or partition. If a subsequent
statement in the transaction is modifying the initially empty table again,
we must enable row-level undo logging, so that we will be able to
roll back to the start of the statement in case of an error (such as
duplicate key).

INSERT IGNORE will continue to use row-level logging and locking, because
implementing it would require the ability to roll back the latest row.
Since the undo log that we write only allows us to roll back the entire
statement, we cannot support INSERT IGNORE. We will introduce a
handler::extra() parameter HA_EXTRA_IGNORE_INSERT to indicate to storage
engines that INSERT IGNORE is being executed.

storage/innobase/row/row0log.cc
@@ -1,7 +1,7 @@
 /*****************************************************************************
 
 Copyright (c) 2011, 2018, Oracle and/or its affiliates. All Rights Reserved.
-Copyright (c) 2017, 2020, MariaDB Corporation.
+Copyright (c) 2017, 2021, MariaDB Corporation.
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
@@ -54,7 +54,9 @@ enum row_tab_op {
 	/** Update a record in place */
 	ROW_T_UPDATE,
 	/** Delete (purge) a record */
-	ROW_T_DELETE
+	ROW_T_DELETE,
+	/** Empty the table */
+	ROW_T_EMPTY
 };
 
 /** Index record modification operations during online index creation */
@@ -62,7 +64,9 @@ enum row_op {
 	/** Insert a record */
 	ROW_OP_INSERT = 0x61,
 	/** Delete a record */
-	ROW_OP_DELETE
+	ROW_OP_DELETE,
+	/** Empy the index */
+	ROW_OP_EMPTY
 };
 
 /** Size of the modification log entry header, in bytes */
@@ -328,7 +332,7 @@ void
 row_log_online_op(
 /*==============*/
 	dict_index_t*	index,	/*!< in/out: index, S or X latched */
-	const dtuple_t* tuple,	/*!< in: index tuple */
+	const dtuple_t*	tuple,	/*!< in: index tuple (NULL=empty the index) */
 	trx_id_t	trx_id)	/*!< in: transaction ID for insert,
 				or 0 for delete */
 {
@@ -339,8 +343,8 @@ row_log_online_op(
 	ulint		avail_size;
 	row_log_t*	log;
 
-	ut_ad(dtuple_validate(tuple));
-	ut_ad(dtuple_get_n_fields(tuple) == dict_index_get_n_fields(index));
+	ut_ad(!tuple || dtuple_validate(tuple));
+	ut_ad(!tuple || dtuple_get_n_fields(tuple) == dict_index_get_n_fields(index));
 	ut_ad(index->lock.have_x() || index->lock.have_s());
 
 	if (index->is_corrupted()) {
@@ -353,14 +357,20 @@ row_log_online_op(
 	row_merge_buf_encode(), because here we do not encode
 	extra_size+1 (and reserve 0 as the end-of-chunk marker). */
 
-	size = rec_get_converted_size_temp(
-		index, tuple->fields, tuple->n_fields, &extra_size);
-	ut_ad(size >= extra_size);
-	ut_ad(size <= sizeof log->tail.buf);
+	if (!tuple) {
+		mrec_size = 4;
+		extra_size = 0;
+		size = 2;
+	} else {
+		size = rec_get_converted_size_temp(
+			index, tuple->fields, tuple->n_fields, &extra_size);
+		ut_ad(size >= extra_size);
+		ut_ad(size <= sizeof log->tail.buf);
 
-	mrec_size = ROW_LOG_HEADER_SIZE
-		+ (extra_size >= 0x80) + size
-		+ (trx_id ? DATA_TRX_ID_LEN : 0);
+		mrec_size = ROW_LOG_HEADER_SIZE
+			+ (extra_size >= 0x80) + size
+			+ (trx_id ? DATA_TRX_ID_LEN : 0);
+	}
 
 	log = index->online_log;
 	mysql_mutex_lock(&log->mutex);
@@ -389,6 +399,8 @@ row_log_online_op(
 		*b++ = ROW_OP_INSERT;
 		trx_write_trx_id(b, trx_id);
 		b += DATA_TRX_ID_LEN;
+	} else if (!tuple) {
+		*b++ = ROW_OP_EMPTY;
 	} else {
 		*b++ = ROW_OP_DELETE;
 	}
@@ -401,8 +413,14 @@ row_log_online_op(
 		*b++ = (byte) extra_size;
 	}
 
-	rec_convert_dtuple_to_temp(
-		b + extra_size, index, tuple->fields, tuple->n_fields);
+	if (tuple) {
+		rec_convert_dtuple_to_temp(
+			b + extra_size, index, tuple->fields,
+			tuple->n_fields);
+	} else {
+		memset(b, 0, 2);
+	}
+
 	b += size;
 
 	if (mrec_size >= avail_size) {
@@ -2422,11 +2440,6 @@ row_log_table_apply_op(
 
 	*error = DB_SUCCESS;
 
-	/* 3 = 1 (op type) + 1 (extra_size) + at least 1 byte payload */
-	if (mrec + 3 >= mrec_end) {
-		return(NULL);
-	}
-
 	const bool is_instant = log->is_instant(dup->index);
 	const mrec_t* const mrec_start = mrec;
 
@@ -2656,6 +2669,11 @@ row_log_table_apply_op(
 		*error = row_log_table_apply_update(
 			thr, new_trx_id_col,
 			mrec, offsets, offsets_heap, heap, dup, old_pk);
+		break;
+	case ROW_T_EMPTY:
+		dup->index->online_log->table->clear(thr);
+		log->head.total += 1;
+		next_mrec = mrec;
 		break;
 	}
 
@@ -3438,6 +3456,9 @@ row_log_apply_op_low(
 			}
 
 			goto duplicate;
+		case ROW_OP_EMPTY:
+			ut_ad(0);
+			break;
 		}
 	} else {
 		switch (op) {
@@ -3507,6 +3528,9 @@ insert_the_rec:
 				&rec, &big_rec,
 				0, NULL, &mtr);
 			ut_ad(!big_rec);
+			break;
+		case ROW_OP_EMPTY:
+			ut_ad(0);
 			break;
 		}
 		mem_heap_empty(offsets_heap);
@@ -3582,6 +3606,16 @@ row_log_apply_op(
 		op = static_cast<enum row_op>(*mrec++);
 		trx_id = 0;
 		break;
+	case ROW_OP_EMPTY:
+	{
+		mem_heap_t* heap = mem_heap_create(512);
+		que_fork_t* fork = que_fork_create(
+			NULL, NULL, QUE_FORK_MYSQL_INTERFACE, heap);
+		que_thr_t* thr = que_thr_create(fork, heap, nullptr);
+		index->clear(thr);
+		mem_heap_free(heap);
+		return mrec + 4;
+	}
 	default:
 corrupted:
 		ut_ad(0);
@@ -4023,4 +4057,55 @@ row_log_apply(
 	row_log_free(log);
 
 	DBUG_RETURN(error);
+}
+
+/** Notify that the table was emptied by concurrent rollback or purge.
+@param index  clustered index */
+static void row_log_table_empty(dict_index_t *index)
+{
+  ut_ad(index->lock.have_any());
+  row_log_t* log= index->online_log;
+  ulint	avail_size;
+  if (byte *b= row_log_table_open(log, 1, &avail_size))
+  {
+    *b++ = ROW_T_EMPTY;
+    row_log_table_close(index, b, 1, avail_size);
+  }
+}
+
+void dict_table_t::clear(que_thr_t *thr)
+{
+  bool rebuild= false;
+  for (dict_index_t *index= UT_LIST_GET_FIRST(indexes); index;
+       index= UT_LIST_GET_NEXT(indexes, index))
+  {
+    if (index->type & DICT_FTS)
+      continue;
+
+    switch (dict_index_get_online_status(index)) {
+    case ONLINE_INDEX_ABORTED:
+    case ONLINE_INDEX_ABORTED_DROPPED:
+      continue;
+
+    case ONLINE_INDEX_COMPLETE:
+      break;
+
+    case ONLINE_INDEX_CREATION:
+      index->lock.u_lock(SRW_LOCK_CALL);
+      if (dict_index_get_online_status(index) == ONLINE_INDEX_CREATION)
+      {
+        if (index->is_clust())
+        {
+          row_log_table_empty(index);
+          rebuild= true;
+        }
+        else if (!rebuild)
+          row_log_online_op(index, nullptr, 0);
+      }
+
+      index->lock.u_unlock();
+    }
+
+    index->clear(thr);
+  }
 }

mysql-test/suite/innodb/r/innodb-table-online.result
@@ -18,15 +18,12 @@ ddl_log_file_alter_table	0
 SET DEBUG_SYNC = 'RESET';
 SET DEBUG_SYNC = 'write_row_noreplace SIGNAL have_handle WAIT_FOR go_ahead';
 INSERT INTO t1 VALUES(1,2,3);
-# Establish session con1 (user=root)
 connect  con1,localhost,root,,;
-connection con1;
 SET DEBUG_SYNC = 'now WAIT_FOR have_handle';
 SET lock_wait_timeout = 1;
 ALTER TABLE t1 ROW_FORMAT=REDUNDANT;
 ERROR HY000: Lock wait timeout exceeded; try restarting transaction
 SET DEBUG_SYNC = 'now SIGNAL go_ahead';
-# session default
 connection default;
 ERROR 23000: Duplicate entry '1' for key 'PRIMARY'
 SELECT name, count FROM INFORMATION_SCHEMA.INNODB_METRICS WHERE subsystem = 'ddl';
@@ -37,7 +34,6 @@ ddl_online_create_index	0
 ddl_pending_alter_table	0
 ddl_sort_file_alter_table	0
 ddl_log_file_alter_table	0
-# session con1
 connection con1;
 SET @saved_debug_dbug = @@SESSION.debug_dbug;
 SET DEBUG_DBUG = '+d,innodb_OOM_prepare_inplace_alter';
@@ -55,7 +51,6 @@ SET SESSION DEBUG = @saved_debug_dbug;
 Warnings:
 Warning	1287	'@@debug' is deprecated and will be removed in a future release. Please use '@@debug_dbug' instead
 ALTER TABLE t1 ROW_FORMAT=REDUNDANT, ALGORITHM=INPLACE, LOCK=NONE;
-# session default
 connection default;
 SHOW CREATE TABLE t1;
 Table	Create Table
@@ -67,22 +62,17 @@ t1	CREATE TABLE `t1` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=REDUNDANT
 BEGIN;
 INSERT INTO t1 VALUES(7,4,2);
-# session con1
 connection con1;
 SET DEBUG_SYNC = 'row_log_table_apply1_before SIGNAL scanned WAIT_FOR insert_done';
 ALTER TABLE t1 DROP PRIMARY KEY, ADD UNIQUE INDEX(c2);
 ERROR HY000: Lock wait timeout exceeded; try restarting transaction
-# session default
 connection default;
 COMMIT;
-# session con1
 connection con1;
 ALTER TABLE t1 DROP PRIMARY KEY, ADD UNIQUE INDEX(c2);
 ERROR 23000: Duplicate entry '4' for key 'c2'
-# session default
 connection default;
 DELETE FROM t1 WHERE c1 = 7;
-# session con1
 connection con1;
 ALTER TABLE t1 DROP PRIMARY KEY, ADD UNIQUE INDEX(c2), ROW_FORMAT=COMPACT,
 LOCK = SHARED, ALGORITHM = INPLACE;
@@ -100,7 +90,6 @@ t1	CREATE TABLE `t1` (
   UNIQUE KEY `c2_2` (`c2`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT
 ALTER TABLE t1 DROP INDEX c2, ADD PRIMARY KEY(c1);
-# session default
 connection default;
 SET DEBUG_SYNC = 'now WAIT_FOR scanned';
 SELECT name, count FROM INFORMATION_SCHEMA.INNODB_METRICS WHERE subsystem = 'ddl';
@@ -114,13 +103,10 @@ ddl_log_file_alter_table	0
 BEGIN;
 INSERT INTO t1 VALUES(4,7,2);
 SET DEBUG_SYNC = 'now SIGNAL insert_done';
-# session con1
 connection con1;
 ERROR 23000: Duplicate entry '4' for key 'PRIMARY'
-# session default
 connection default;
 ROLLBACK;
-# session con1
 connection con1;
 SHOW CREATE TABLE t1;
 Table	Create Table
@@ -142,7 +128,6 @@ ddl_online_create_index	0
 ddl_pending_alter_table	0
 ddl_sort_file_alter_table	0
 ddl_log_file_alter_table	0
-# session default
 connection default;
 INSERT INTO t1 VALUES(6,3,1);
 ERROR 23000: Duplicate entry '3' for key 'c2_2'
@@ -152,14 +137,12 @@ DROP INDEX c2_2 ON t1;
 BEGIN;
 INSERT INTO t1 VALUES(7,4,2);
 ROLLBACK;
-# session con1
 connection con1;
 KILL QUERY @id;
 ERROR 70100: Query execution was interrupted
 SET DEBUG_SYNC = 'row_log_table_apply1_before SIGNAL rebuilt WAIT_FOR dml_done';
 SET DEBUG_SYNC = 'row_log_table_apply2_before SIGNAL applied WAIT_FOR kill_done';
 ALTER TABLE t1 ROW_FORMAT=REDUNDANT;
-# session default
 connection default;
 SET DEBUG_SYNC = 'now WAIT_FOR rebuilt';
 SELECT name, count FROM INFORMATION_SCHEMA.INNODB_METRICS WHERE subsystem = 'ddl';
@@ -176,7 +159,6 @@ ROLLBACK;
 SET DEBUG_SYNC = 'now SIGNAL dml_done WAIT_FOR applied';
 KILL QUERY @id;
 SET DEBUG_SYNC = 'now SIGNAL kill_done';
-# session con1
 connection con1;
 ERROR 70100: Query execution was interrupted
 SELECT name, count FROM INFORMATION_SCHEMA.INNODB_METRICS WHERE subsystem = 'ddl';
@@ -187,7 +169,6 @@ ddl_online_create_index	0
 ddl_pending_alter_table	0
 ddl_sort_file_alter_table	0
 ddl_log_file_alter_table	0
-# session default
 connection default;
 CHECK TABLE t1;
 Table	Op	Msg_type	Msg_text
@@ -212,7 +193,6 @@ WHERE variable_name = 'innodb_encryption_n_merge_blocks_decrypted');
 SET @rowlog_encrypt_0=
 (SELECT variable_value FROM information_schema.global_status
 WHERE variable_name = 'innodb_encryption_n_rowlog_blocks_encrypted');
-# session con1
 connection con1;
 SHOW CREATE TABLE t1;
 Table	Create Table
@@ -227,7 +207,6 @@ SET DEBUG_SYNC = 'row_log_table_apply1_before SIGNAL rebuilt2 WAIT_FOR dml2_done
 SET lock_wait_timeout = 10;
 ALTER TABLE t1 ROW_FORMAT=COMPACT
 PAGE_COMPRESSED = YES PAGE_COMPRESSION_LEVEL = 1, ALGORITHM = INPLACE;
-# session default
 connection default;
 INSERT INTO t1 SELECT  80 + c1, c2, c3 FROM t1;
 INSERT INTO t1 SELECT 160 + c1, c2, c3 FROM t1;
@@ -290,7 +269,6 @@ SELECT
 sort_balance	@merge_encrypt_1>@merge_encrypt_0	@merge_decrypt_1>@merge_decrypt_0	@rowlog_encrypt_1>@rowlog_encrypt_0
 0	0	0	0
 SET DEBUG_SYNC = 'now SIGNAL dml2_done';
-# session con1
 connection con1;
 ERROR HY000: Creating index 'PRIMARY' required more than 'innodb_online_alter_log_max_size' bytes of modification log. Please try again
 SELECT name, count FROM INFORMATION_SCHEMA.INNODB_METRICS WHERE subsystem = 'ddl';
@@ -321,7 +299,6 @@ ERROR 23000: Duplicate entry '5' for key 'PRIMARY'
 ALTER TABLE t1 DROP PRIMARY KEY, ADD PRIMARY KEY(c22f,c1,c4(5)),
 CHANGE c2 c22f INT, CHANGE c3 c3 CHAR(255) NULL, CHANGE c1 c1 INT AFTER c22f,
 ADD COLUMN c4 VARCHAR(6) DEFAULT 'Online', LOCK=NONE;
-# session default
 connection default;
 SET DEBUG_SYNC = 'now WAIT_FOR rebuilt3';
 SELECT name, count FROM INFORMATION_SCHEMA.INNODB_METRICS WHERE subsystem = 'ddl';
@@ -349,7 +326,6 @@ ddl_pending_alter_table	1
 ddl_sort_file_alter_table	2
 ddl_log_file_alter_table	2
 SET DEBUG_SYNC = 'now SIGNAL dml3_done';
-# session con1
 connection con1;
 SELECT name, count FROM INFORMATION_SCHEMA.INNODB_METRICS WHERE subsystem = 'ddl';
 name	count
@@ -405,20 +381,16 @@ SET DEBUG_SYNC = 'row_log_table_apply1_before SIGNAL c3p5_created0 WAIT_FOR ins_
 ALTER TABLE t1 MODIFY c3 CHAR(255) NOT NULL, DROP COLUMN c22f,
 DROP PRIMARY KEY, ADD PRIMARY KEY(c1,c4(5)),
 ADD COLUMN c5 CHAR(5) DEFAULT 'tired' FIRST;
-# session default
 connection default;
 SET DEBUG_SYNC = 'now WAIT_FOR c3p5_created0';
 BEGIN;
 INSERT INTO t1 VALUES(347,33101,'Pikku kakkosen posti','YLETV2');
 INSERT INTO t1 VALUES(33101,347,NULL,'');
 SET DEBUG_SYNC = 'now SIGNAL ins_done0';
-# session con1
 connection con1;
 ERROR 01000: Data truncated for column 'c3' at row 323
-# session default
 connection default;
 ROLLBACK;
-# session con1
 connection con1;
 ALTER TABLE t1 MODIFY c3 CHAR(255) NOT NULL;
 SET DEBUG_SYNC = 'row_log_table_apply1_before SIGNAL c3p5_created WAIT_FOR ins_done';
@@ -426,14 +398,12 @@ ALTER TABLE t1 DROP PRIMARY KEY, DROP COLUMN c22f,
 ADD COLUMN c6 VARCHAR(1000) DEFAULT
 'I love tracking down hard-to-reproduce bugs.',
 ADD PRIMARY KEY c3p5(c3(5), c6(2));
-# session default
 connection default;
 SET DEBUG_SYNC = 'now WAIT_FOR c3p5_created';
 SET DEBUG_SYNC = 'ib_after_row_insert SIGNAL ins_done WAIT_FOR ddl_timed_out';
 INSERT INTO t1 VALUES(347,33101,NULL,'');
 ERROR 23000: Column 'c3' cannot be null
 INSERT INTO t1 VALUES(347,33101,'Pikku kakkosen posti','');
-# session con1
 connection con1;
 ERROR HY000: Lock wait timeout exceeded; try restarting transaction
 SET DEBUG_SYNC = 'now SIGNAL ddl_timed_out';
@@ -445,7 +415,6 @@ ddl_online_create_index	0
 ddl_pending_alter_table	0
 ddl_sort_file_alter_table	6
 ddl_log_file_alter_table	2
-# session default
 connection default;
 SELECT COUNT(*) FROM t1;
 COUNT(*)
@@ -463,12 +432,8 @@ c22f	c1	c3	c4
 5	36	36foofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoo	Online
 5	41	41foofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoo	Online
 5	46	46foofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoofoo	Online
-# session con1
 connection con1;
 ALTER TABLE t1 DISCARD TABLESPACE;
-# Disconnect session con1
-disconnect con1;
-# session default
 connection default;
 SHOW CREATE TABLE t1;
 Table	Create Table
@@ -479,9 +444,25 @@ t1	CREATE TABLE `t1` (
   `c4` varchar(6) NOT NULL DEFAULT 'Online',
   PRIMARY KEY (`c22f`,`c1`,`c4`(5))
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=REDUNDANT
-SET DEBUG_SYNC = 'RESET';
 SET GLOBAL innodb_monitor_disable = module_ddl;
 DROP TABLE t1;
+CREATE TABLE t1 (a INT PRIMARY KEY) ENGINE=InnoDB;
+connection con1;
+SET DEBUG_SYNC = 'row_log_table_apply1_before SIGNAL created WAIT_FOR ins';
+ALTER TABLE t1 FORCE;
+connection default;
+SET DEBUG_SYNC = 'now WAIT_FOR created';
+BEGIN;
+INSERT INTO t1 VALUES(1);
+ROLLBACK;
+SET DEBUG_SYNC = 'now SIGNAL ins';
+connection con1;
+disconnect con1;
+connection default;
+SELECT * FROM t1;
+a
+DROP TABLE t1;
+SET DEBUG_SYNC = 'RESET';
 SET GLOBAL innodb_file_per_table = @global_innodb_file_per_table_orig;
 SET GLOBAL innodb_monitor_enable  = default;
 SET GLOBAL innodb_monitor_disable = default;

mysql-test/suite/innodb/t/innodb-table-online.test
@@ -30,9 +30,7 @@ SET DEBUG_SYNC = 'write_row_noreplace SIGNAL have_handle WAIT_FOR go_ahead';
 --send
 INSERT INTO t1 VALUES(1,2,3);
 
---echo # Establish session con1 (user=root)
 connect (con1,localhost,root,,);
-connection con1;
 
 # This should block at the end because of the INSERT in connection default
 # is holding a metadata lock.
@@ -42,13 +40,11 @@ SET lock_wait_timeout = 1;
 ALTER TABLE t1 ROW_FORMAT=REDUNDANT;
 SET DEBUG_SYNC = 'now SIGNAL go_ahead';
 
---echo # session default
 connection default;
 --error ER_DUP_ENTRY
 reap;
 eval $innodb_metrics_select;
 
---echo # session con1
 connection con1;
 SET @saved_debug_dbug = @@SESSION.debug_dbug;
 SET DEBUG_DBUG = '+d,innodb_OOM_prepare_inplace_alter';
@@ -61,14 +57,12 @@ ALTER TABLE t1 ROW_FORMAT=REDUNDANT, ALGORITHM=INPLACE, LOCK=NONE;
 SET SESSION DEBUG = @saved_debug_dbug;
 ALTER TABLE t1 ROW_FORMAT=REDUNDANT, ALGORITHM=INPLACE, LOCK=NONE;
 
---echo # session default
 connection default;
 SHOW CREATE TABLE t1;
 # Insert a duplicate entry (4) for the upcoming UNIQUE INDEX(c2).
 BEGIN;
 INSERT INTO t1 VALUES(7,4,2);
 
---echo # session con1
 connection con1;
 # This DEBUG_SYNC should not kick in yet, because the duplicate key will be
 # detected before we get a chance to apply the online log.
@@ -78,20 +72,16 @@ SET DEBUG_SYNC = 'row_log_table_apply1_before SIGNAL scanned WAIT_FOR insert_don
 --error ER_LOCK_WAIT_TIMEOUT
 ALTER TABLE t1 DROP PRIMARY KEY, ADD UNIQUE INDEX(c2);
 
---echo # session default
 connection default;
 COMMIT;
 
---echo # session con1
 connection con1;
 --error ER_DUP_ENTRY
 ALTER TABLE t1 DROP PRIMARY KEY, ADD UNIQUE INDEX(c2);
 
---echo # session default
 connection default;
 DELETE FROM t1 WHERE c1 = 7;
 
---echo # session con1
 connection con1;
 ALTER TABLE t1 DROP PRIMARY KEY, ADD UNIQUE INDEX(c2), ROW_FORMAT=COMPACT,
 LOCK = SHARED, ALGORITHM = INPLACE;
@@ -106,7 +96,6 @@ SHOW CREATE TABLE t1;
 --send
 ALTER TABLE t1 DROP INDEX c2, ADD PRIMARY KEY(c1);
 
---echo # session default
 connection default;
 SET DEBUG_SYNC = 'now WAIT_FOR scanned';
 eval $innodb_metrics_select;
@@ -116,7 +105,6 @@ BEGIN;
 INSERT INTO t1 VALUES(4,7,2);
 SET DEBUG_SYNC = 'now SIGNAL insert_done';
 
---echo # session con1
 connection con1;
 # Because the modification log will be applied in order and we did
 # not roll back before the log apply, there will be a duplicate key
@@ -124,11 +112,9 @@ connection con1;
 --error ER_DUP_ENTRY
 reap;
 
---echo # session default
 connection default;
 ROLLBACK;
 
---echo # session con1
 connection con1;
 SHOW CREATE TABLE t1;
 # Now, rebuild the table without any concurrent DML, while no duplicate exists.
@@ -137,7 +123,6 @@ ALTER TABLE t1 DROP PRIMARY KEY, ADD UNIQUE INDEX(c2), ALGORITHM = INPLACE;
 ALTER TABLE t1 DROP INDEX c2, ADD PRIMARY KEY(c1), ALGORITHM = INPLACE;
 eval $innodb_metrics_select;
 
---echo # session default
 connection default;
 --error ER_DUP_ENTRY
 INSERT INTO t1 VALUES(6,3,1);
@@ -148,7 +133,6 @@ BEGIN;
 INSERT INTO t1 VALUES(7,4,2);
 ROLLBACK;
 
---echo # session con1
 connection con1;
 let $ID= `SELECT @id := CONNECTION_ID()`;
 --error ER_QUERY_INTERRUPTED
@@ -159,7 +143,6 @@ SET DEBUG_SYNC = 'row_log_table_apply2_before SIGNAL applied WAIT_FOR kill_done'
 --send
 ALTER TABLE t1 ROW_FORMAT=REDUNDANT;
 
---echo # session default
 connection default;
 SET DEBUG_SYNC = 'now WAIT_FOR rebuilt';
 eval $innodb_metrics_select;
@@ -171,13 +154,11 @@ let $ignore= `SELECT @id := $ID`;
 KILL QUERY @id;
 SET DEBUG_SYNC = 'now SIGNAL kill_done';
 
---echo # session con1
 connection con1;
 --error ER_QUERY_INTERRUPTED
 reap;
 eval $innodb_metrics_select;
 
---echo # session default
 connection default;
 CHECK TABLE t1;
 INSERT INTO t1 SELECT  5 + c1, c2, c3 FROM t1;
@@ -199,7 +180,6 @@ SET @rowlog_encrypt_0=
 (SELECT variable_value FROM information_schema.global_status
 WHERE variable_name = 'innodb_encryption_n_rowlog_blocks_encrypted');
 
---echo # session con1
 connection con1;
 SHOW CREATE TABLE t1;
 ALTER TABLE t1 ROW_FORMAT=REDUNDANT;
@@ -217,7 +197,6 @@ PAGE_COMPRESSED = YES PAGE_COMPRESSION_LEVEL = 1, ALGORITHM = INPLACE;
 # Generate some log (delete-mark, delete-unmark, insert etc.)
 # while the index creation is blocked. Some of this may run
 # in parallel with the clustered index scan.
---echo # session default
 connection default;
 INSERT INTO t1 SELECT  80 + c1, c2, c3 FROM t1;
 INSERT INTO t1 SELECT 160 + c1, c2, c3 FROM t1;
@@ -261,7 +240,6 @@ SELECT
 # Release con1.
 SET DEBUG_SYNC = 'now SIGNAL dml2_done';
 
---echo # session con1
 connection con1;
 # If the following fails with the wrong error, it probably means that
 # you should rerun with a larger mtr --debug-sync-timeout.
@@ -295,7 +273,6 @@ ALTER TABLE t1 DROP PRIMARY KEY, ADD PRIMARY KEY(c22f,c1,c4(5)),
 CHANGE c2 c22f INT, CHANGE c3 c3 CHAR(255) NULL, CHANGE c1 c1 INT AFTER c22f,
 ADD COLUMN c4 VARCHAR(6) DEFAULT 'Online', LOCK=NONE;
 
---echo # session default
 connection default;
 SET DEBUG_SYNC = 'now WAIT_FOR rebuilt3';
 # Generate some log (delete-mark, delete-unmark, insert etc.)
@@ -312,7 +289,6 @@ eval $innodb_metrics_select;
 # Release con1.
 SET DEBUG_SYNC = 'now SIGNAL dml3_done';
 
---echo # session con1
 connection con1;
 reap;
 eval $innodb_metrics_select;
@@ -365,7 +341,6 @@ ALTER TABLE t1 MODIFY c3 CHAR(255) NOT NULL, DROP COLUMN c22f,
 DROP PRIMARY KEY, ADD PRIMARY KEY(c1,c4(5)),
 ADD COLUMN c5 CHAR(5) DEFAULT 'tired' FIRST;
 
---echo # session default
 connection default;
 
 SET DEBUG_SYNC = 'now WAIT_FOR c3p5_created0';
@@ -374,16 +349,13 @@ INSERT INTO t1 VALUES(347,33101,'Pikku kakkosen posti','YLETV2');
 INSERT INTO t1 VALUES(33101,347,NULL,'');
 SET DEBUG_SYNC = 'now SIGNAL ins_done0';
 
---echo # session con1
 connection con1;
 --error WARN_DATA_TRUNCATED
 reap;
 
---echo # session default
 connection default;
 ROLLBACK;
 
---echo # session con1
 connection con1;
 ALTER TABLE t1 MODIFY c3 CHAR(255) NOT NULL;
 
@@ -394,7 +366,6 @@ ADD COLUMN c6 VARCHAR(1000) DEFAULT
 'I love tracking down hard-to-reproduce bugs.',
 ADD PRIMARY KEY c3p5(c3(5), c6(2));
 
---echo # session default
 connection default;
 SET DEBUG_SYNC = 'now WAIT_FOR c3p5_created';
 SET DEBUG_SYNC = 'ib_after_row_insert SIGNAL ins_done WAIT_FOR ddl_timed_out';
@@ -403,32 +374,47 @@ INSERT INTO t1 VALUES(347,33101,NULL,'');
 --send
 INSERT INTO t1 VALUES(347,33101,'Pikku kakkosen posti','');
 
---echo # session con1
 connection con1;
 --error ER_LOCK_WAIT_TIMEOUT
 reap;
 SET DEBUG_SYNC = 'now SIGNAL ddl_timed_out';
 eval $innodb_metrics_select;
 
---echo # session default
 connection default;
 reap;
 SELECT COUNT(*) FROM t1;
 ALTER TABLE t1 ROW_FORMAT=REDUNDANT;
 SELECT * FROM t1 LIMIT 10;
 
---echo # session con1
 connection con1;
 ALTER TABLE t1 DISCARD TABLESPACE;
---echo # Disconnect session con1
-disconnect con1;
 
---echo # session default
 connection default;
 SHOW CREATE TABLE t1;
-SET DEBUG_SYNC = 'RESET';
 SET GLOBAL innodb_monitor_disable = module_ddl;
 DROP TABLE t1;
+
+CREATE TABLE t1 (a INT PRIMARY KEY) ENGINE=InnoDB;
+
+connection con1;
+SET DEBUG_SYNC = 'row_log_table_apply1_before SIGNAL created WAIT_FOR ins';
+send ALTER TABLE t1 FORCE;
+
+connection default;
+SET DEBUG_SYNC = 'now WAIT_FOR created';
+BEGIN;
+INSERT INTO t1 VALUES(1);
+ROLLBACK;
+SET DEBUG_SYNC = 'now SIGNAL ins';
+
+connection con1;
+reap;
+disconnect con1;
+
+connection default;
+SELECT * FROM t1;
+DROP TABLE t1;
+SET DEBUG_SYNC = 'RESET';
 
 # Check that all connections opened by test cases in this file are really
 # gone so execution of other tests won't be affected by their presence.

commit 69d49756cbaf4c807e58723055145deaf66dd060

MDEV-24781 Assertion `mode == 16 || mode == 12 || fix_block->page.status != buf_page_t::FREED' failed in buf_page_get_low
This is caused by commit 3cef4f8f0fc88ae5bfae4603d8d600ec84cc70a9
(MDEV-515). dict_table_t::clear() frees all the blob during
rollback of bulk insert.But online log tries to read the
freed blob while applying the log. It can be fixed if we
truncate the online log during rollback of bulk insert operation.

storage/innobase/row/row0log.cc
@@ -326,6 +326,25 @@ row_log_block_free(
 	DBUG_VOID_RETURN;
 }
 
+/** Empty the online log.
+@param index	index log to be cleared */
+static void row_log_empty(dict_index_t *index)
+{
+  ut_ad(index->lock.have_s());
+  row_log_t *log= index->online_log;
+
+  mysql_mutex_lock(&log->mutex);
+  UT_DELETE(log->blobs);
+  log->blobs= NULL;
+  row_log_block_free(log->tail);
+  row_log_block_free(log->head);
+  row_merge_file_destroy_low(log->fd);
+  log->fd= OS_FILE_CLOSED;
+  log->tail.total= log->tail.blocks= log->tail.bytes= 0;
+  log->head.total= log->head.blocks= log->head.bytes= 0;
+  mysql_mutex_unlock(&log->mutex);
+}
+
 /******************************************************//**
 Logs an operation to a secondary index that is (or was) being created. */
 void
@@ -358,6 +377,7 @@ row_log_online_op(
 	extra_size+1 (and reserve 0 as the end-of-chunk marker). */
 
 	if (!tuple) {
+		row_log_empty(index);
 		mrec_size = 4;
 		extra_size = 0;
 		size = 2;
@@ -4063,6 +4083,7 @@ row_log_apply(
 static void row_log_table_empty(dict_index_t *index)
 {
   ut_ad(index->lock.have_s());
+  row_log_empty(index);
   row_log_t* log= index->online_log;
   ulint	avail_size;
   if (byte *b= row_log_table_open(log, 1, &avail_size))

mysql-test/suite/innodb/r/innodb-table-online.result
@@ -446,21 +446,21 @@ t1	CREATE TABLE `t1` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=REDUNDANT
 SET GLOBAL innodb_monitor_disable = module_ddl;
 DROP TABLE t1;
-CREATE TABLE t1 (a INT PRIMARY KEY) ENGINE=InnoDB;
+CREATE TABLE t1 (a INT PRIMARY KEY, b blob) ENGINE=InnoDB;
 connection con1;
 SET DEBUG_SYNC = 'row_log_table_apply1_before SIGNAL created WAIT_FOR ins';
 ALTER TABLE t1 FORCE;
 connection default;
 SET DEBUG_SYNC = 'now WAIT_FOR created';
 BEGIN;
-INSERT INTO t1 VALUES(1);
+INSERT INTO t1 VALUES(1, repeat('a', 10000));
 ROLLBACK;
 SET DEBUG_SYNC = 'now SIGNAL ins';
 connection con1;
 disconnect con1;
 connection default;
 SELECT * FROM t1;
-a
+a	b
 DROP TABLE t1;
 SET DEBUG_SYNC = 'RESET';
 SET GLOBAL innodb_file_per_table = @global_innodb_file_per_table_orig;

mysql-test/suite/innodb/t/innodb-table-online.test
@@ -394,7 +394,7 @@ SHOW CREATE TABLE t1;
 SET GLOBAL innodb_monitor_disable = module_ddl;
 DROP TABLE t1;
 
-CREATE TABLE t1 (a INT PRIMARY KEY) ENGINE=InnoDB;
+CREATE TABLE t1 (a INT PRIMARY KEY, b blob) ENGINE=InnoDB;
 
 connection con1;
 SET DEBUG_SYNC = 'row_log_table_apply1_before SIGNAL created WAIT_FOR ins';
@@ -403,7 +403,7 @@ send ALTER TABLE t1 FORCE;
 connection default;
 SET DEBUG_SYNC = 'now WAIT_FOR created';
 BEGIN;
-INSERT INTO t1 VALUES(1);
+INSERT INTO t1 VALUES(1, repeat('a', 10000));
 ROLLBACK;
 SET DEBUG_SYNC = 'now SIGNAL ins';
 